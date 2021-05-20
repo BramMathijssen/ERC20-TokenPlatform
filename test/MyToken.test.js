76d28eb6a54c0ee91aeffbb66b4b8a91c20ddec8
Bram Mathijssen <Bram.mathijssen@gmail.com>
2021-05-20 13:58:20 +0200

    added more tests

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -11,17 +11,36 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async(accounts) =>{
+
+    // manier om accounts netjes te definen, javascript zorgt automatisch dat
+    // deployerAccount = account 0, recipient = account 1, anotherAccount =2 ..etc.
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("All tokens should be in my account", async() => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     }),
-    it("All tokens should be in my account2", async() => {
+
+    it("I can send tokens from Account 1 to Account 2", async () => {
+        const sendTokens = 1;
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        let balance = await instance.balanceOf(accounts[0]);
-        assert.equal(balance.valueOf(), initialSupply.valueOf(),"The balance was not the same")
-    })
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;      
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+      }),
+
+    it("It's not possible to send more tokens than account 1 has", async () => {
+        let instance = await Token.deployed();
+        let balanceOfAccount = await instance.balanceOf(deployerAccount);
+  
+        expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+  
+        //check if the balance is still the same
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+      });
     
 }
 )
