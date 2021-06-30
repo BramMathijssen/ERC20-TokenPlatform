fa7752d00b28be080ca6dc6efd7f028e7ed7a9b4
Bram Mathijssen <Bram.mathijssen@gmail.com>
2021-06-30 16:50:08 +0200

    updated token purchase test to include KYC

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,5 +1,6 @@
 const Token = artifacts.require("MyToken");
 const TokenSale = artifacts.require("MyTokenSale");
+const KycContract = artifacts.require("KycContract");
 
 const chai = require("./setupchai.js");
 const BN = web3.utils.BN;
@@ -24,15 +25,19 @@ contract("TokenSale Test", async function (accounts) {
         let totalSupply = await instance.totalSupply.call();
         return expect(balance).to.be.a.bignumber.equal(totalSupply);
     });
-    
+
     it("should be possible to buy one token by simply sending ether to the smart contract", async () => {
         let tokenInstance = await Token.deployed();
         let tokenSaleInstance = await TokenSale.deployed();
         let balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient);
-    
-        await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        await expect(tokenSaleInstance.sendTransaction({ from: recipient, value: web3.utils.toWei("1", "wei") })).to.be.rejected;
+        await expect(balanceBeforeAccount).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
+
+        let kycInstance = await KycContract.deployed();
+        await kycInstance.setKycCompleted(recipient);
+        await expect(tokenSaleInstance.sendTransaction({ from: recipient, value: web3.utils.toWei("1", "wei") })).to.be.fulfilled;
         return expect(balanceBeforeAccount + 1).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
-    
+
     });
 
 });
